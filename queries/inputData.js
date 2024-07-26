0d18ec5465d7b1f7024df78d6cb0512bbbefded0
Henry <henrytest2000@gmail.com>
2024-07-26 12:23:46 -0600

    nueva db

--- queries/inputData.js
+++ queries/inputData.js
@@ -18,7 +18,81 @@ const insertUser = async (username, passwordHash, email, role) => {
     throw error;  // Propagar el error para manejarlo en un nivel superior
   }
 };
-// INSERTAR UN LIBRO
+
+// QUERIES PARA TABLA CATEGORIAS
+
+async function insertCategory(name) {
+  const query = `
+      INSERT INTO categories (name)
+      VALUES ($1)
+      RETURNING *;
+  `;
+  const values = [name];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error creating category', err);
+  }
+}
+
+async function getAllCategories() {
+  const query = `
+      SELECT * FROM categories;
+  `;
+  try {
+      const res = await pool.query(query);
+      return res.rows;
+  } catch (err) {
+      console.error('Error fetching categories', err);
+  }
+}
+
+async function getCategoryById(id) {
+  const query = `
+      SELECT * FROM categories WHERE id = $1;
+  `;
+  const values = [id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error fetching category', err);
+  }
+}
+
+async function updateCategory(id, name) {
+  const query = `
+      UPDATE categories
+      SET name = $1
+      WHERE id = $2
+      RETURNING *;
+  `;
+  const values = [name, id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error updating category', err);
+  }
+}
+
+async function deleteCategory(id) {
+  const query = `
+      DELETE FROM categories
+      WHERE id = $1
+      RETURNING *;
+  `;
+  const values = [id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error deleting category', err);
+  }
+}
+
+// QUERIES PARA TALBA BOOKS
 
 async function insertBook(title, author, isbn, publication_year, available_copies) {
   const query = `
@@ -34,6 +108,240 @@ async function insertBook(title, author, isbn, publication_year, available_copie
       console.error('Error creating book', err);
   }
 }
+
+async function getAllBooks() {
+  const query = `
+      SELECT * FROM books;
+  `;
+  try {
+      const res = await pool.query(query);
+      return res.rows;
+  } catch (err) {
+      console.error('Error fetching books', err);
+  }
+}
+
+async function getBookById(id) {
+  const query = `
+      SELECT * FROM books WHERE id = $1;
+  `;
+  const values = [id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error fetching book', err);
+  }
+}
+
+async function updateBook(id, title, author, isbn, publication_year, available_copies) {
+  const query = `
+      UPDATE books
+      SET title = $1, author = $2, isbn = $3, publication_year = $4, available_copies = $5
+      WHERE id = $6
+      RETURNING *;
+  `;
+  const values = [title, author, isbn, publication_year, available_copies, id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error updating book', err);
+  }
+}
+
+async function deleteBook(id) {
+  const query = `
+      DELETE FROM books
+      WHERE id = $1
+      RETURNING *;
+  `;
+  const values = [id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error deleting book', err);
+  }
+}
+
+//  QUERIES PARA LA TABLA FAVORITES
+
+async function insertFavorite(user_id, book_id) {
+  const query = `
+      INSERT INTO favorites (user_id, book_id)
+      VALUES ($1, $2)
+      RETURNING *;
+  `;
+  const values = [user_id, book_id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error creating favorite', err);
+  }
+}
+
+async function getAllFavoritesByUser(user_id) {
+  const query = `
+      SELECT * FROM favorites WHERE user_id = $1;
+  `;
+  const values = [user_id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows;
+  } catch (err) {
+      console.error('Error fetching favorites', err);
+  }
+}
+
+async function getAllUsersByFavoriteBook(book_id) {
+  const query = `
+      SELECT * FROM favorites WHERE book_id = $1;
+  `;
+  const values = [book_id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows;
+  } catch (err) {
+      console.error('Error fetching favorite users', err);
+  }
+}
+
+async function deleteFavorite(user_id, book_id) {
+  const query = `
+      DELETE FROM favorites
+      WHERE user_id = $1 AND book_id = $2
+      RETURNING *;
+  `;
+  const values = [user_id, book_id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error deleting favorite', err);
+  }
+}
+
+
+// QUERIES PARA LAS ORDERS
+
+async function insertOrder(user_id, book_id, loan_date, return_date) {
+  const query = `
+      INSERT INTO orders (user_id, book_id, loan_date, return_date)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+  `;
+  const values = [user_id, book_id, loan_date, return_date];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error creating order', err);
+  }
+}
+
+async function getAllOrders() {
+  const query = `
+      SELECT * FROM orders;
+  `;
+  try {
+      const res = await pool.query(query);
+      return res.rows;
+  } catch (err) {
+      console.error('Error fetching orders', err);
+  }
+}
+
+async function getOrderById(id) {
+  const query = `
+      SELECT * FROM orders WHERE id = $1;
+  `;
+  const values = [id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error fetching order', err);
+  }
+}
+
+async function updateOrder(id, user_id, book_id, loan_date, return_date) {
+  const query = `
+      UPDATE orders
+      SET user_id = $1, book_id = $2, loan_date = $3, return_date = $4
+      WHERE id = $5
+      RETURNING *;
+  `;
+  const values = [user_id, book_id, loan_date, return_date, id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error updating order', err);
+  }
+}
+
+async function deleteOrder(id) {
+  const query = `
+      DELETE FROM orders
+      WHERE id = $1
+      RETURNING *;
+  `;
+  const values = [id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error deleting order', err);
+  }
+}
+
+// QUERIES PARA LA TABLA BOOKS CATEGORIES
+
+async function insertBookCategory(book_id, category_id) {
+  const query = `
+      INSERT INTO book_categories (book_id, category_id)
+      VALUES ($1, $2)
+      RETURNING *;
+  `;
+  const values = [book_id, category_id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error creating book-category relationship', err);
+  }
+}
+
+async function getBooksByCategoryId(category_id) {
+  const query = `
+      SELECT * FROM book_categories WHERE category_id = $1;
+  `;
+  const values = [category_id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows;
+  } catch (err) {
+      console.error('Error fetching books for category', err);
+  }
+}
+
+async function deleteBookCategory(book_id, category_id) {
+  const query = `
+      DELETE FROM book_categories
+      WHERE book_id = $1 AND category_id = $2
+      RETURNING *;
+  `;
+  const values = [book_id, category_id];
+  try {
+      const res = await pool.query(query, values);
+      return res.rows[0];
+  } catch (err) {
+      console.error('Error deleting book-category relationship', err);
+  }
+}
+
 
 
 // Ejemplos de uso
